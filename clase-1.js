b2c366c21984ffbeffefac175e881338ac4d9432
ivan diaz <owivan.91@gmail.com>
2023-06-06 18:47:43 -0600

    add new class

---
+++ clase-1.js
@@ -0,0 +1,69 @@
+
+
+let number = 23123123; // numero
+let text = '2';
+
+let booleno = true; // booleano
+
+let other = undefined;
+
+lastName = 870
+
+//typeof evalua el tipo de dato
+
+console.log(typeof text)
+
+//Operadores
+
+// Asignacion 
+// =
+
+// Aritmeticos
+// adicion +
+// sustraccion -
+// multiplicacion *
+// division /
+// exponenciacion **
+// Modulo %
+// incremento ++
+// Decremento --
+// Logicos
+
+let name = 'ivan';
+let lastName = 'Diaz'; // string
+
+console.log(2 + '2' )
+
+console.log(name + ' ' + lastName )
+
+// concatenar
+
+console.log(13 % 5)
+console.log(11 % 2)
+
+let edad = 6;
+
+// edad++ 
+// edad--
+// edad--
+// && and
+// || or 
+// > mayor que 
+// < menor que
+// == comparacion
+// === comparacion estricta
+
+// console.log(edad < 18 && edad < 5)
+// console.log(edad < 18 || edad < 5 || edad > 0 )
+
+// console.log(2 == '2')
+// console.log(2 === '2')
+
+// let age = 18;
+// if(age >= 18){
+//  console.log('es una persona mayor edad')
+// }else{
+//     console.log('es menor de edad')
+// }
+
+
